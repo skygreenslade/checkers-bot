649a46da26d27f55f9da03c69105ea1ee1a87dd5
Sky Greenslade <skygreenslade@gmail.com>
2023-03-12 16:55:05 -0500

    get user input and parse into motor, radToMove

--- calib/src/main.cpp
+++ calib/src/main.cpp
@@ -4,6 +4,15 @@
 #include "MeEEPROM.h"
 #include <HardwareSerial.h>
 
+#define SLOW_SPEED 40
+
+#define TICKS_PER_FULL_ROTATION_1   2576
+#define TICKS_PER_FULL_ROTATION_2   3312
+
+#define ARM_1_LENGTH 35.5
+#define ARM_2_LENGTH 25
+
+
 #define ENCODER_BOARD 61
   //Read type
   #define ENCODER_BOARD_POS    0x01
@@ -29,12 +38,117 @@ unsigned long time;
 unsigned long last_time;
 
 MeMegaPiDCMotor motor1(PORT1B);
+MeMegaPiDCMotor motor2(PORT1A);
 
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 
 
 volatile long encoder_pos1 = 0;
+volatile long encoder_pos2 = 0;
+
+struct motorInfo {
+    MeMegaPiDCMotor *motor;
+    MeEncoderOnBoard *encoder;
+    volatile long *position;
+};
+
+
+// void movePos(motorInfo toMove, long target, uint16_t speed);
+// void movePosSlow(motorInfo toMove, long target);
+// void moveNeg(motorInfo toMove, long target, uint16_t speed);
+// void moveNegSlow(motorInfo toMove, long target);
+
+
+// //main move function
+// void moveTo(int motorNum, long target, uint16_t speed){
+
+//     struct motorInfo toMove;
+
+//     //get motor info for given motor number
+//     switch(motorNum){
+//         case 1:
+//             toMove.motor = &motor1;
+//             toMove.encoder = &Encoder_1;      // Give me a pointer!!!
+//             toMove.position = &encoder_pos1;
+//             break;
+//         case 2:
+//             toMove.motor = &motor2;
+//             toMove.encoder = &Encoder_2;
+//             toMove.position = &encoder_pos2;
+//             break;
+//         // case 3:
+//         //     toMove.motor = motor3;
+//         //     toMove.encoder = Encoder_3;
+//         //     toMove.position = encoder_pos3;
+//         //     break;
+//         default:
+//         //uh oh
+//     }
+
+//     //positive case
+//     if (target > toMove.position)
+//         movePos(toMove, target, speed);
+//     //negative case
+//     else
+//         moveNeg(toMove, target, speed);
+
+// }//moveTo
+
+
+
+// void movePos(motorInfo toMove, long target, uint16_t speed){
+
+//     //move until at or past target position - 50 units
+//     while(target - 50 >= *toMove.position){
+//         (*toMove.motor).run(abs(speed));
+//     }
+
+//     //slow down for final bit
+//     movePosSlow(toMove, target);
+
+
+// }//movePos
+
+
+// void movePosSlow(motorInfo toMove, long target){
+
+//     int speed = SLOW_SPEED;
+
+//     //move until at or past target position
+//     while(target >= *toMove.position){
+//         (*toMove.motor).run(abs(speed));
+//     }
+
+// }//movePosSlow
+
+
+// void moveNeg(motorInfo toMove, long target, uint16_t speed){
+
+//     //move until at or past target position - 50 units
+//     while(target + 50 <= *toMove.position){
+//         (*toMove.motor).run(-abs(speed));
+//     }
+
+//     //slow down for final bit
+//     movePosSlow(toMove, target);
+
+
+// }//moveNeg
+
+
+// void moveNegSlow(motorInfo toMove, long target){
+
+//     int speed = SLOW_SPEED;
+
+//     //move until at or past target position
+//     while(target <= *toMove.position){
+//         (*toMove.motor).run(-abs(speed));
+//     }
+
+// }//moveNegSlow
+
+
 
 void isr_process_encoder1(void)
 {
@@ -42,13 +156,14 @@ void isr_process_encoder1(void)
   if(digitalRead(Encoder_1.getPortB()) == 0)
   {
     //Serial.println("hit interupt + 1");
-    encoder_pos1 -= 1;
+    Encoder_1.pulsePosMinus();
   }
   else
   {
     //Serial.println("hit interupt - 1");
-    encoder_pos1 += 1;
+    Encoder_1.pulsePosPlus();
   }
+
 }
 
 void isr_process_encoder2(void)
@@ -63,17 +178,33 @@ void isr_process_encoder2(void)
   }
 }
 
-void setup()
-{
-  attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
-  attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
-  pinMode(interruptPin, INPUT_PULLUP);
-  pinMode(NE1, INPUT);
-  Serial.begin(9600);   
+bool notbeencalled = 1;
+
+void changeDir(){
+
+  //Encoder_1.move(414, 200, 0, (cb) changeDir);
+}
+
+void changeDir2(){
+  //Encoder_2.move(-322, 200, 1, (cb) changeDir2);
+}
+
+// Takes the requested position as input and returns the bot's coordinates needed to accompilsh that. 
+// theta 2 is the offset of theta1.
+// 
+void getBotAngles( double x, double y, double *theta1, double *theta2){
+
+  double l1 = ARM_1_LENGTH;
+  double l2 = ARM_2_LENGTH;
+
+  double r = sqrt(pow(x,2) + pow(y,2));   // Get the hypotenus. 
+  *theta2 = acos( ( pow(r, 2) - pow(l1, 2) - pow(l2, 2) ) / 2*l1*l2 );            // compute theta 2 (offset angle of theta 1)
+  *theta1 = atan(y/x) - atan( (l2 * sin(*theta2)) / (l1 + l2*cos(*theta2) ));     // compute theta 1
 }
 
 
-//moves relative given amount of distance, at given speed
+
+
 void moveTo(long distToMove, uint16_t speed){
 
     long startPos = encoder_pos1;
@@ -122,6 +253,36 @@ void moveto(long dist, uint16_t motorSpeed){
 
 }
 
+
+
+
+void setup()
+{
+  attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
+  attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+  pinMode(interruptPin, INPUT_PULLUP);
+  pinMode(NE1, INPUT);
+  Serial.begin(9600);   
+
+  double theta1, theta2;
+
+  // getBotAngles(15, -15, &theta1, &theta2);
+
+  // int ticks1 = (int) theta1 * TICKS_PER_FULL_ROTATION_1;
+  // int ticks2 = (int) theta2 * TICKS_PER_FULL_ROTATION_2;
+
+  // Serial.println(ticks1);
+  // Serial.println(ticks2);
+
+
+  // Encoder_1.move(ticks1, 200, 0, (cb) changeDir);
+  // Encoder_2.move(ticks2, 200, 1, (cb) changeDir2);
+}
+
+
+
+
+
 void loop()
 {
 
@@ -130,16 +291,85 @@ void loop()
 
     Serial.print("Current:");
     Serial.println(pos1);
-    while(1){
-      
-      moveto(200, 50);
-      delay(1000);
-      moveto(-200, 50);
-      delay(1000);
 
-    }
+    char* input;  //buffer for user input
+    int input1;  //holds input data for which motor to move
+    int input2;  //holds input data for how far to move
+    bool inputValid = false; //boolean for whether input is valid or not
+
+    int motorToMove;  //which motor number to move
+    int degToMove;    //degrees to move
+    long radToMove;   //degrees to move converted to radians
+
+    //get user input for motor to move, and how much
+    while (!inputValid){
+      //wait for user input
+      while(Serial.available() == 0);
+
+      //read in input, assume format: <motorNum>,<rotationInDegrees>
+      strcpy(input, (Serial.readStringUntil(',')).c_str());
+      input1 = strtol(input, '\0', 0);
+      strcpy(input, (Serial.readString()).c_str());
+      input2 = strtol(input, '\0', 0);
+
+      //ensure motor selection is 1, 2, or 3
+      switch(input1){
+        case(1):
+          motorToMove = 1;
+          inputValid = true;
+          break;
+        case(2):
+          motorToMove = 2;
+          inputValid = true;
+          break;
+        case(3):
+          motorToMove = 3;
+          inputValid = true;
+        default:
+          Serial.println("Please give motor number as 1, 2, or 3, followed by a comma.");
+          inputValid = false;
+      }
+
+      //ensure that angle to move is within desired range
+      if(-180<=input2 && input2<=180){
+        degToMove = input2;
+        inputValid = true; 
+      }
+      else{
+        Serial.println("Please give motor movement as degree between +/- 180.");
+        inputValid = false;
+      }
+
+      if(!inputValid){
+        Serial.println("Example: 2,-90");
+      }
+
     
-    motor1.stop();
-  
+    }//waiting for valid input
+
+    //output what input was
+    Serial.print("moving motor ");
+    Serial.print(char(motorToMove));
+    Serial.print(", ");
+    Serial.print(char(degToMove));
+    Serial.println(" degrees.");
+
+    //approximate radians to move
+    radToMove = (degToMove*71)/4068
+
+
+
+
+
+
+    // while(1){
+
+    //   Encoder_1.loop();
+    //   Encoder_2.loop();
+
+      //Encoder_2.setMotorPwm(-100);
+
+    // }
+      
 }
 
