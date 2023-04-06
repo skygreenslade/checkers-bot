65600fb5b6ad071ca364fb5ab135d7539221af7f
Sky Greenslade <skygreenslade@gmail.com>
2023-04-06 18:45:25 -0500

    preserve old versions of number/lettering

---
+++ python/checkersBoardOld.py
@@ -0,0 +1,510 @@
+
+import os, sys #FIFOs
+
+# 0 = blank
+# 2 = robot
+# 3 = opponent
+# 22 = robot king piece
+# 33 = opponent king piece
+
+
+#fifo to write robot's (legal) moves to
+fifoPath = "../moves"
+fifo = None     #holds fd for fifo
+
+#file which holds board state
+boardFile = "./boardState.ini" 
+
+
+# top of board is away from robot
+newBoard =   [[0, 3, 0, 3, 0, 3, 0, 3],
+              [3, 0, 3, 0, 3, 0, 3, 0],
+              [0, 3, 0, 3, 0, 3, 0, 3],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [2, 0, 2, 0, 2, 0, 2, 0],
+              [0, 2, 0, 2, 0, 2, 0, 2],
+              [2, 0, 2, 0, 2, 0, 2, 0]]
+
+
+
+
+#################### functions ########################
+
+
+
+#read board state from file and return it as board matrix
+def readBoard(boardFile):
+
+    newState = newBoard
+    newRow = 0 #position for new board
+    newCol = -1 #position ofr new board
+
+    #read in board file
+    file = open(boardFile, "r")
+    state = file.read()
+    nextChar = state[1]
+
+    #parse for checkers positions
+    for i in range(0,len(state)-1):
+        char = state[i]
+        nextChar = state[i+1]
+        
+        #next checkers position reached
+        if char == '[':
+            #move to next square
+            newCol+=2
+            if newCol == 9:
+                newRow += 1
+                newCol = 0
+            elif newCol == 8:
+                newRow += 1
+                newCol = 1
+            
+            #update board state
+            newState[newRow][newCol] = convertFromPiece(nextChar)
+        
+    return newState
+
+#readBoard
+
+
+
+
+
+#write board state to given file boardFile
+def writeBoard(board, boardFile):
+
+    row = ""    #holds row to be printed
+    rowLetter = ord('H')    #letter to pre/append to each row
+    numRow = "   1  2  3  4  5  6  7  8"    # header and footer rows
+
+    #open file
+    file = open(boardFile, "w")
+
+    #print header row of numbers
+    file.write(numRow)
+    file.write("\n")
+
+    #for each row
+    for i in range(0,8):
+        #add row letter to front of row
+        row = chr(rowLetter)+ ' '
+
+        #for each position in row
+        for j in range (0,8):
+
+            #unused spaces are blank
+            if ((j+i)%2) == 0:
+                row += "   "
+
+            #add appropriate character to each space
+            else:
+                row += convertPiece(board[i][j])
+        
+        #add row letter to end of row an print row
+        row += ' ' + chr(rowLetter)
+        file.write(row)
+        file.write("\n")
+        #decremenmt row letter for next row
+        rowLetter -= 1
+    
+    #bottom row of numbers  
+    file.write(numRow)
+    file.write("\n")
+    
+#writeBoard
+
+
+
+
+
+
+
+#converts given character into approporiate piece
+#r = robot, o = oppoent, capital = king piece
+#
+def convertPiece(piece):
+
+    toRet = "[ ]"
+
+    #convert to appropriate output
+    if piece == 2:
+        toRet = "[r]"
+    elif piece == 3:
+        toRet = "[o]"
+    elif piece == 22:
+        toRet = "[R]"
+    elif piece == 33:
+        toRet = "[O]"
+
+    return toRet
+
+#convertPiece
+
+
+
+
+#converts piece into code
+# r, R into 2, 22. o, O into 3, 33. blank into 0
+def convertFromPiece(piece):
+
+    toRet = 0
+
+    if piece == 'r':
+        toRet = 2
+    if piece == 'R':
+        toRet = 22
+    if piece == 'o':
+        toRet = 3
+    if piece == 'O':
+        toRet = 33
+    
+    return toRet
+
+#convertFromPiece
+
+
+
+
+#outputs current state of given board in terminal
+def printBoard(board):
+
+    row = ""    #holds row to be printed
+    rowLetter = ord('H')    #letter to pre/append to each row
+    numRow = "   1  2  3  4  5  6  7  8"    # header and footer rows
+
+    #print header row of numbers
+    print(numRow)
+
+    #for each row
+    for i in range(0,8):
+        #add row letter to front of row
+        row = chr(rowLetter)+ ' '
+
+        #for each position in row
+        for j in range (0,8):
+
+            #unused spaces are blank
+            if ((j+i)%2) == 0:
+                row += "   "
+
+            #add appropriate character to each space
+            else:
+                row += convertPiece(board[i][j])
+        
+        #add row letter to end of row an print row
+        row += ' ' + chr(rowLetter)
+        print(row)
+        #decremenmt row letter for next row
+        rowLetter -= 1
+    
+    #bottom row of numbers  
+    print(numRow)
+
+#printBoard
+
+
+#returns int of row corresponding to the letter given
+#returns None on invalid input
+def getRow(letter):
+
+    row = None
+
+    if letter == 'H' or letter == 'h':
+        row = 0
+    elif letter == 'G' or letter == 'g':
+        row = 1
+    elif letter == 'F' or letter == 'f':
+        row = 2
+    elif letter == 'E' or letter == 'e':
+        row = 3
+    elif letter == 'D' or letter == 'd':
+        row = 4
+    elif letter == 'C' or letter == 'c':
+        row = 5
+    elif letter == 'B' or letter == 'b':
+        row = 6
+    elif letter == 'A' or letter == 'a':
+        row = 7
+
+
+    return row
+#getRow
+
+
+#returns int of column corresponding to the number system
+#returns None on invalid input
+def getCol(letter):
+
+    col = None
+
+    #convert to int, subtract one for 0 based indexing
+    try:
+        col = int(letter) - 1
+    except:
+        pass
+
+    # set to Null if out of bounds
+    if (not isinstance(col, int)) or col > 8 or col < 0:
+        col = None
+    
+    return col
+#getCol
+
+
+#moves piece at oldPos to newPos on given board (if movement is valid)
+def move(oldPos, newPos, board):
+
+    error = 0           #error code for function
+    capture = False     #flag of if a piece was captured
+    kingPiece = False   #boolean of if piece is a king piece or not
+    piece = 0           #holds piece
+
+    #hold coordinates of old and new position for piece
+    oldRow = None
+    newRow = None
+    oldCol = None
+    newCol = None
+
+    #ensure arguments are long enough
+    if len(oldPos) > 1 and len(newPos) > 1: 
+        #convert given positions into array indices
+        oldRow = getRow(oldPos[0])
+        newRow = getRow(newPos[0])
+        oldCol = getCol(oldPos[1])
+        newCol = getCol(newPos[1])
+
+    #check that given values were valid
+    if oldRow is None or oldCol is None or oldCol is None or newCol is None:
+        error  = 1 #invalid input
+    elif isinstance(oldCol, type(None)) or isinstance(newCol, type(None)) or isinstance(oldRow, type(None)) or isinstance(newRow, type(None)):
+        error = 1 #invalid input
+
+    #check that the move only involves legal squares
+    elif ((oldRow+oldCol)%2) == 0 or ((newRow+newCol)%2) == 0:
+        error = 1 #invalid input
+
+    #check if there is already a piece at new location
+    elif boardState[newRow][newCol] != 0:
+        error = 2 #illegal move: piece already at new location
+
+    #proceed if no error is found
+    if not error:
+
+        #get piece to move
+        piece = boardState[oldRow][oldCol]
+
+        #check if piece is a king piece
+        if piece > 10:
+            kingPiece = True
+    
+        #check if move is legal for king piece
+    
+        #if a king piece is not moving one square diagonally 
+        if kingPiece and (abs(oldRow-newRow) != 1 or abs(oldCol-newCol) != 1):
+            
+            # if piece did not "jump" over a space diagonally 
+            if abs(oldRow-newRow) != 2 or abs(oldCol - newCol) != 2:
+                error = 3 #illegal move: not how pieces move
+
+            #if piece did not jump over an opponent piece
+            elif boardState[(oldRow+newRow)//2][(oldCol+newCol)//2]%(piece%10) == 0:
+                error = 4 #illegal move: no piece to capture
+
+                #if prev cases are false, piece moved diagonally, over an opponent piece
+            else:
+                capture = True
+        
+        elif not kingPiece:
+            
+            #robot piece case, when not moving one space diagonally
+            if piece == 2 and ((oldRow - newRow) != 1 or abs(oldCol - newCol) != 1):
+                
+                #if piece did not jump diagonally in the correct direction
+                if (oldRow - newRow) != 2 or abs (oldCol - newCol) != 2:
+                    error = 3 #illegal move: not how pieces move
+
+
+                #if piece did not jump over an opponent piece
+                elif boardState[(oldRow+newRow)//2][(oldCol+newCol)//2]%(piece%10) == 0:
+                    error = 4 #illegal move: no piece to capture 
+
+                #if previous cases are false, piece moved diagonally in the right direction, over an opponent piece 
+                else:
+                    capture = True
+            
+            #opponent piece case, when not moving one space diagonally
+            elif piece == 3 and ((oldRow - newRow) != -1 or abs(oldCol - newCol) != 1):
+
+                #if piece did not jump diagonally in the correct direction
+                if (oldRow - newRow) != -2 or abs (oldCol - newCol) != 2:
+                    error = 3 #illegal move: not how pieces move
+
+
+                #if piece did not jump over an opponent piece
+                elif boardState[(oldRow+newRow)//2][(oldCol+newCol)//2]%(piece%10) == 0:
+                    error = 4 #illegal move: no piece to capture 
+
+                #if previous cases are false, piece moved diagonally in the right direction, over an opponent piece 
+                else:
+                    capture = True
+
+
+    #if no error, execute move
+    if not error:
+        
+        #move piece from old location to new location
+        piece = boardState[oldRow][oldCol]
+        boardState[oldRow][oldCol] = 0
+        boardState[newRow][newCol] = piece
+        
+        #remove piece if one was captured
+        if capture:
+            boardState[(oldRow+newRow)//2][(oldCol+newCol)//2] = 0
+
+        #make king piece if piece made it to opposite end
+        if piece == 2 and newRow == 0:
+            boardState[newRow][newCol]*=11
+        elif piece == 3 and newRow == 7:
+            boardState[newRow][newCol]*=11
+
+
+        #write to FIFO if move was a legal robot move
+        if piece%3 != 0:
+            output = cmd + ' ' + oldPos + ' ' + newPos + '\n'
+            oputMove(output)
+            
+
+    return error
+#move
+
+
+
+#outputs given string to given fifo
+def oputMove(toOput):
+    print("writing to fifo: ",toOput)
+
+    #write to fifo
+    fifo.write(toOput)
+    fifo.flush()
+
+#oputMove
+
+
+
+
+
+
+
+
+#################### main ######################
+
+
+exit = False
+
+
+
+print("Welcome to the Checkers Board Script.")
+
+new = input("Press '0' for new board\n")
+
+
+if new == '0':
+    boardState = newBoard
+else:
+    boardState = readBoard(boardFile)
+    
+printBoard(boardState)
+
+
+#create and open FIFO
+try:
+    os.mkfifo(fifoPath)
+except OSError as err:
+    print("Failed to create FIFO, %s" % err)
+try:
+    print("opening FIFO. Will wait for reader.")
+    fifo = open(fifoPath, 'w', 1)
+except OSError as err:
+    print("Error opening file, %s", err)
+
+
+
+
+
+
+
+#run until user exits
+while exit == False:
+
+
+    # get user input
+    inp = input("Enter a command. 'x' to exit.\n")
+
+    # get command from input
+    params = inp.split(' ')
+    cmd = params[0]
+
+    # act on given input
+    #exit case
+    if cmd == 'x':
+        exit = True
+    
+    #move piece command
+    elif cmd == 'mv':
+        toMove = params[1]
+        moveTo = params[2] 
+        
+        error = move(toMove, moveTo, boardState)
+
+        if error == 1:
+            print("Error: invalid input.")
+        elif error == 2:
+            print("Error: invalid move. Piece already at target destination")
+        elif error == 3:
+            print("Error: invalid move. Pieces must move one space diagonally, or two when capturing")
+            print("Only king pieces may capture backwards.")
+        elif error == 4:
+            print("Error: invalid move. No opponent piece to capture")
+
+
+        #output board state
+        printBoard(boardState)
+
+        #update board file with new state
+        writeBoard(newBoard, boardFile)
+
+
+
+    #print board state command
+    elif cmd == 'b':
+        printBoard(boardState)
+
+
+ 
+    #help command
+    elif cmd == "h" or cmd == "help":
+        print("commands:")
+        print("\t x,\t\t Exit program")
+        print("\t mv,\t\t Move piece from coord X# to X#, eg: mv C1 D2")
+        print("\t b, \t\t Print current board state")
+        print("\t h, help,\t display this message")
+
+
+    #default
+    else:
+        print("Sorry, that command is unrecognized. Enter \"help\" for help.")
+
+
+
+
+
+print("\nProgram completed normally. Exiting.\n")
+#end of main
+
+
+
+
+
+
---
+++ python/commandConverterOld.py
@@ -0,0 +1,231 @@
+
+
+import math
+import testSerial
+import time
+
+#FIFO path
+fifoPath = "../moves"
+
+#distances in mm
+LENGTHX = 40 #side length of square in x direction
+LENGTHY = 40 #side length of square in y direction
+A1CENTER = ( 0 + LENGTHX/2, 87+73 - 5 + LENGTHY/2) #center of A1 square on x-axis
+
+#lengths of arms in mm
+ARM_1 = 348 - 9
+ARM_2 = 225 - 5    #was 225
+
+
+
+
+
+########################### FUNCTIONS ############################
+
+
+#returns int of row corresponding to the letter given
+#returns None on invalid input
+def getRow(letter):
+
+    row = None
+
+    if letter == 'H' or letter == 'h':
+        row = 7
+    elif letter == 'G' or letter == 'g':
+        row = 6
+    elif letter == 'F' or letter == 'f':
+        row = 5
+    elif letter == 'E' or letter == 'e':
+        row = 4
+    elif letter == 'D' or letter == 'd':
+        row = 3
+    elif letter == 'C' or letter == 'c':
+        row = 2
+    elif letter == 'B' or letter == 'b':
+        row = 1
+    elif letter == 'A' or letter == 'a':
+        row = 0
+
+
+    return row
+#getRow
+
+
+#returns int of column corresponding to the number system
+#returns None on invalid input
+def getCol(letter):
+
+    col = None
+
+    if letter == '1':
+        col = 0
+    elif letter == '2':
+        col = 1
+    elif letter == '3':
+        col = 2
+    elif letter == '4':
+        col = 3
+    elif letter == '5':
+        col = 4
+    elif letter == '6':
+        col = 5
+    elif letter == '7':
+        col = 6
+    elif letter == '8':
+        col = 7
+
+    return col
+#getCol
+
+
+
+#returns theta 1 and theta 2 for given tuple x,y coords
+#uses constants defined at top of script
+def invKin(coordinates):
+    x = coordinates[0]
+    y = coordinates[1]
+    
+    #top and bottom term for theta 2 calculation
+    topTerm = (x**2) + (y**2) - (ARM_1**2) - (ARM_2**2)
+    bottomTerm = 2*ARM_1*ARM_2
+
+    #calculate theta 2
+    t2 = -math.acos(topTerm/bottomTerm)
+
+    #top and bottom term for theta 1 calculation
+    topTerm = ARM_2*math.sin(t2)
+    bottomTerm = (ARM_1+(ARM_2*math.cos(t2)))
+    
+    #calculat theta 1
+    t1 = math.atan(y/x) - math.atan(topTerm/bottomTerm)
+
+    # topTerm = (x**2) + (y**2) + (ARM_1**2) - (ARM_2**2)
+    # bottomTerm = (2 * math.sqrt((x**2)+(y**2)) * ARM_1)
+
+    # t1 = math.atan(y/x) + math.atan(topTerm/bottomTerm) 
+
+
+    return(t1, t2)
+#invKin
+
+
+
+#changes radians angles to those expected by the arduino
+def convertRad(thetas):
+
+    t1 = math.pi - thetas[0]
+    t2 = -thetas[1]
+
+    t1 = 180*t1/math.pi
+    t2 = 180*t2/math.pi
+
+    return (t1, t2)
+
+#convertRad
+
+
+
+
+def wait_for_arduino():
+    arduino_ready = False
+    while arduino_ready is not True:
+        arduino_ready = testSerial.read_serial()
+
+    print("Arduino Ready!")
+    
+#wait_for_arduino
+
+
+
+
+#executes the commands for a move from thetas1 to thetas2
+def movePiece(thetas1, thetas2):
+
+    testSerial.move_robot(float(thetas1[0]), float(thetas1[1]))
+    wait_for_arduino()
+    time.sleep(1)
+    testSerial.pickup(float(thetas1[0]), float(thetas1[1]))
+    wait_for_arduino()
+    time.sleep(1)
+    testSerial.move_robot(float(thetas2[0]), float(thetas2[1]))
+    wait_for_arduino()
+    time.sleep(1)
+    testSerial.drop(float(thetas2[0]), float(thetas2[1]))
+    wait_for_arduino()
+    time.sleep(1)
+#movePiece
+
+
+
+
+
+############################ main #############################
+
+
+exit = False
+#x,y position of start of move, and end of move
+pos1 = (None, None)
+pos2 = (None, None)
+
+#open FIFO
+print("Opening FIFO")
+
+try:
+    fifo = open(fifoPath, "r")
+except OSError as err: 
+    print("Could not open FIFO, %s", err)
+else:
+    print("FIFO opened for reading.")
+
+
+#read in from FIFO until closed
+while not exit:
+    cmd = fifo.readline()
+
+    #if FIFO is closed, exit
+    if len(cmd) == 0:
+        exit = True
+        print("FIFO closed from writer side.")
+    
+    #parse command
+    else:
+
+        #parse input. NO ERROR HANDLING (only valid commands passed from checkersBoard.py)
+        params = cmd.split(' ')
+        oldPos = params[1]
+        newPos = params[2]
+
+
+        oldRow = getRow(oldPos[0])
+        newRow = getRow(newPos[0])
+        oldCol = getCol(oldPos[1])
+        newCol = getCol(newPos[1])
+
+        #calculate x,y cordinates of each position
+        pos1 = (oldCol*LENGTHX + A1CENTER[0], oldRow*LENGTHY + A1CENTER[1])
+        pos2 = (newCol*LENGTHX + A1CENTER[0], newRow*LENGTHY + A1CENTER[1])
+
+        #calculate thetas for each position
+        thetas1 = invKin(pos1)
+        thetas2 = invKin(pos2)
+
+        print(pos1)
+
+        # print(thetas1[0]*180/math.pi, thetas1[1]*180/math.pi, thetas2[0]*180/math.pi, thetas2[1]*180/math.pi)
+        print(thetas1[0]/math.pi, thetas1[1]/math.pi, thetas2[0]/math.pi, thetas2[1]/math.pi)
+        # print(thetas1, thetas2)
+
+        #convert thetas to appropriate values
+        thetas1 = convertRad(thetas1)   
+        thetas2 = convertRad(thetas2)
+
+        movePiece(thetas1, thetas2)
+
+
+print("\nProgram completed normally. Exiting.\n")
+#main
+
+
+
+
+
