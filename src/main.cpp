ee8a09292ce9a4b0561a2046dcf9527871dc276c
Sky Greenslade <skygreenslade@gmail.com>
2023-04-11 21:27:09 -0500

    start of toggle gripper state routing

--- src/main.cpp
+++ src/main.cpp
@@ -637,3 +637,40 @@ void loop()
 
 }
 
+
+enum gripper_state{
+  GRIPPER_OPEN,
+  GRIPPER_CLOSED
+};
+
+int gripperState = GRIPPER_OPEN;
+
+void toggleGripper(){
+
+  switch(gripperState){
+    case GRIPPER_OPEN:
+    //move to closed state
+
+      Gripper.run(250);
+      Encoder_3.setMotorPwm(0);
+      delay(900);
+      Gripper.run(0);
+  
+      gripperState = GRIPPER_CLOSED;   // trigger this once gripped
+      break;
+
+    case GRIPPER_CLOSED:
+      Serial.println("RAISED_GRIPPING");
+      Gripper.run(-250);
+      Encoder_3.setMotorPwm(0);
+      delay(900);
+      Gripper.run(0);
+
+      break;
+
+
+  }
+
+
+}
+
